778021494896459bb2aa294074654f390db83e4f
dlottermann <diegolottermann@gmail.com>
2024-09-17 22:55:40 -0300

    add pinecone

--- pinecone.js
+++ pinecone.js
@@ -1,4 +1,9 @@
 import { Pinecone } from '@pinecone-database/pinecone';
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { PDFLoader } from "langchain/community/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -7,8 +12,14 @@ const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
 });
 
-const index = pc.index('nlp');
 
-const stats = await index.describeIndexStats();
+const loader = new DirectoryLoader("./documents", {
+  ".txt": (path) => new TextLoader(path),
+  ".pdf": (path) => new PDFLoader(path),
+});
+const docs = await loader.load();
+
+
+const stats = await pc.listIndexes();
 
 console.log(stats)
