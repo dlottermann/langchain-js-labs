fda242cd3b57c29f69d522811546f36f41cc341c
dlottermann <diegolottermann@gmail.com>
2024-09-18 09:36:13 -0300

    add templates example

--- templates.js
+++ templates.js
@@ -1,5 +1,5 @@
-import { PromptTemplate } from "@langchain/core/prompts";
-import { OpenAI } from "@langchain/openai";
+import { PromptTemplate, ChatPromptTemplate } from "@langchain/core/prompts";
+import { OpenAI, ChatOpenAI } from "@langchain/openai";
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -17,3 +17,26 @@ const openai = new OpenAI({
 
 const response = await openai.invoke(prompt);
 console.log("Saída do LLM:\n", response);
+
+console.log(
+  "\n-------------------------------------------------------------CHAT model-------------------------------------------------------------\n"
+);
+
+const chatPromptTemplate = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant"],
+  ["user", "Tell me a joke about {topic}"],
+]);
+
+const chatPrompt = await chatPromptTemplate.invoke({ topic: "cats" });
+
+const openaiChat = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  maxtokens: 500,
+});
+
+const chatResponse = await openaiChat.invoke(chatPrompt);
+console.log("Saída do LLM:\n", chatResponse.content);
+
+console.log(
+  "\n-------------------------------------------------------------End Of Script -------------------------------------------------------------\n"
+);
